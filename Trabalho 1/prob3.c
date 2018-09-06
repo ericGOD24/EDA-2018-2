dbbeaca629851067c47d9861e47847209fc59370
ericGOD24 <ericobandeira@hotmail.com>
2018-09-06 09:53:05 -0300

    add a function in prob3

--- Trabalho 1/prob3.c
+++ Trabalho 1/prob3.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Declarando Função
+void hanoi (int, char, char, char);
+int movimentos(int);
+
 //========================================================================
 int main (int argc, char *argv[]){
   int discos;
 
-  printf("Forneca o numero de discos");
-  scanf("%d\n", &discos);
-
+  printf("Forneca o numero de discos: \n");
+  scanf("%d", &discos);
+  if(discos < 1){
+    while(discos < 1){
+      printf("Valor invalido. Insira novamente: \n");
+      scanf("%d", &discos);
+    }
+  }
+  printf("numero de movimentos: %d\n", movimentos(discos));
   hanoi (discos, 'A', 'C', 'B');
   return 0;
 }
@@ -21,4 +31,13 @@ void hanoi (int d, char ori, char dest, char aux){
       printf("Mover disco %d de %c para %c\n", d, ori, dest);
       hanoi(d-1, aux, dest, ori);
     }
+}
+
+int movimentos(int discos){
+  if (discos == 1){
+    return 1;
+  }
+  else{
+    return 2*movimentos(discos-1) + 1;
+  }
 }
